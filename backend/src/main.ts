2f0bf129adf49d5d4a9abf75f38db5c4cb5b2fa8
amaurialmeida <amauri.almeidasjr@gmail.com>
2026-02-11 13:49:43 -0200

    Adiciona definição da porta

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
+﻿import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AUTH_MESSAGES } from './auth/messages/auth.message';
 import { validationExceptionFactory } from './common/exceptions/validation.factory';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -39,8 +39,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port, function () {
-    console.log(`BACKEND is running on port ${port}.`);
+  await app.listen(port, '0.0.0.0', function () { // ✅ ADICIONADO '0.0.0.0'
+    console.log(`🚀 BACKEND is running on port ${port}`);
   });
 }
 void bootstrap();
